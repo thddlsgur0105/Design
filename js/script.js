0ac67818579f6135e0b63155981d136838249774
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-07-01 18:25:24 +0900

    Updated Circle

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 class ActiveCircle {
-    constructor({size, color, radius, top, left}) {
-        this.circle = document.querySelector("#jsCircle");
-        this.circle.style.width = size;
-        this.circle.style.height = size;
-        this.circle.style.backgroundColor = color;
-        this.circle.style.borderRadius = radius;
-        this.circle.style.top = top;
-        this.circle.style.left = left;
+    constructor(css) {
+        this.createCircle(css);
         this.activate();
+    }
+    createCircle = ({size, color, radius, top, left, text}) => {
+        this.main = document.getElementById("main");
+        this.circle = document.createElement("button");
+        if (text) {
+            this.circle.innerHTML = text;
+        }
+        this.circle.style.cssText = `
+            width: ${size};
+            height: ${size};
+            background-color: ${color};
+            border-radius: ${radius};
+            top: ${top};
+            left: ${left};
+            position: absolute;
+            box-shadow: 2px 2px 2px 0px #34495e;
+            font-size: 10px;
+            font-weight: 600;
+            color: #2c3e50;
+        `
+        this.main.appendChild(this.circle);
     }
     activate = () => {
         this.circle.addEventListener("mousedown", this.handleBtnDown);
@@ -27,26 +42,60 @@ class ActiveCircle {
         const btn = event.target;
         if (btn.classList.contains("down-btn")) {
             btn.classList.replace("down-btn", "up-btn");
-            
-            // Create continuously expanding Circle
-            this.createCircle(event)
         }
-    }
-    createCircle = (event) => {
-        const x = event.target.offsetLeft + 50;
-        const y = event.target.offsetTop + 50;
-        console.log(x, y)
-        this.createElement()
-    }
-    createElement(element, css) {
-        const newElement = document.createElement(element);
     }
 }
 
-const circle = new ActiveCircle({
+// CIRCLE AREA
+
+const circle1 = new ActiveCircle({
     size: "120px",
-    color: "#2ecc71",
+    color: "#A1FAFF",
     radius: "50%",
     top: "500px",
     left: "1000px",
+});
+
+const circle2 = new ActiveCircle({
+    size: "170px",
+    color: "#4BA5FF",
+    radius: "50%",
+    top: "300px",
+    left: "700px",
+});
+
+const circle3 = new ActiveCircle({
+    size: "50px",
+    color: "#3E48FF",
+    radius: "50%",
+    top: "100px",
+    left: "300px",
+});
+
+const circle4 = new ActiveCircle({
+    size: "130px",
+    color: "#3969E8",
+    radius: "50%",
+    top: "100px",
+    left: "450px",
+});
+
+// FORM AREA
+
+const form1 = new ActiveCircle({
+    size: "100px",
+    color: "#ecf0f1",
+    radius: "10%",
+    top: "10px",
+    left: "10px",
+    text: "POWERFUL"
+});
+
+const form2 = new ActiveCircle({
+    size: "100px",
+    color: "#ecf0f1",
+    radius: "10%",
+    top: "120px",
+    left: "10px",
+    text: "COOL"
 });
