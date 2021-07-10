58fc5962fe2ce80c7be69ad2d310387568c0074e
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-07-10 17:50:54 +0900

    Canvas 진행중

--- js/script-two.js
+++ js/script-two.js
@@ -1,9 +1,28 @@
-const tutorial = document.getElementById("tutorial");
-if (tutorial.getContext) {
-    const ctx = tutorial.getContext("2d");
-    // drawing code here
-    ctx.fillStyle = "green";
-    ctx.fillRect(10, 10, 150, 100)
-} else {
-    // canvas unsupported code here
+const load = () => {
+    const tutorial = document.getElementById("tutorial");
+    if (tutorial.getContext) {
+        const ctx = tutorial.getContext("2d");
+        // drawing code here
+        ctx.fillStyle = "green";
+
+        // Filled triangle
+        ctx.beginPath();
+        ctx.moveTo(25, 25);
+        ctx.lineTo(105, 25);
+        ctx.lineTo(25, 105);
+        ctx.fill();
+
+        // Stroked triangle
+        ctx.beginPath()
+        ctx.moveTo(125, 125);
+        ctx.lineTo(125, 45);
+        ctx.lineTo(45, 125);
+        ctx.closePath();
+        ctx.stroke();
+
+    } else {
+        // canvas unsupported code here
+    }
 }
+
+window.onload = load;
